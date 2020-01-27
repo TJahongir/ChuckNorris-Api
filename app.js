912d347694c13c9a682531e9cc1c93d14b9ae977
Jahongir <jakhongirt@gmail.com>
2020-01-27 11:09:20 +0500

    First

---
+++ app.js
@@ -0,0 +1,21 @@
+const jokeForm = document.getElementById('jokeForm');
+const jokeText = document.getElementById('jokeText');
+const firstNameInput = document.getElementById('firstName');
+const lastNameInput = document.getElementById('lastName');
+
+jokeForm.addEventListener('submit',(e) => {
+    e.preventDefault();
+    const firstName = firstNameInput.value || 'Chuck';
+    const lastName = lastNameInput.value || 'Norris';
+
+    const url = `https://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`;
+    fetch(url)
+        .then( res => {
+            return res.json();
+            
+        }).then(data => {
+            console.log(data);
+            jokeText.innerHTML = data.value.joke;
+            jokeText.classList.add('jokeText');
+        })
+});
